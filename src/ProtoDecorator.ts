9d0a227b740684453a1971b1c3404cf98fee86f8
Arturas Molcanovas <a.molcanovas@gmail.com>
2017-11-19 06:12:51 +0000

    tests

--- src/ProtoDecorator.ts
+++ src/ProtoDecorator.ts
@@ -1,3 +1,10 @@
+/**
+ * Sets a value on the class' prototype
+ * @param value The value to set
+ * @param configurable Whether the value should be configurable
+ * @param enumerable Whether the value should be enumerable
+ * @return {(target: any, prop: string) => void}
+ */
 export function Proto(value: any, configurable = true, enumerable = true): (target: any, prop: string) => void {
   return function(target: any, propertyKey: string) {
     Object.defineProperty(target.constructor.prototype, propertyKey, {
--- test/Proto.ts
+++ test/Proto.ts
@@ -1,5 +1,54 @@
 import {GenericTestContext, test} from 'ava';
+import {Proto} from '../src/ProtoDecorator';
 
-test('stub', (t: GenericTestContext<any>) => {
-  t.true(true);
+class Foo {
+
+  @Proto('def')
+  public defaults: string;
+  @Proto('b', false, true)
+  public enumNoConfig: string;
+  @Proto('a', true, false)
+  public noEnumConfig: string;
+}
+
+const desc = {
+  defaults:     Object.getOwnPropertyDescriptor(Foo.prototype, 'defaults'),
+  enumNoConfig: Object.getOwnPropertyDescriptor(Foo.prototype, 'enumNoConfig'),
+  noEnumConfig: Object.getOwnPropertyDescriptor(Foo.prototype, 'noEnumConfig')
+};
+
+test('Default value', (t: GenericTestContext<any>) => {
+  t.is(Foo.prototype.defaults, 'def');
+});
+
+test('Default configurable', (t: GenericTestContext<any>) => {
+  t.true(desc.defaults.configurable);
+});
+
+test('Default enumerable', (t: GenericTestContext<any>) => {
+  t.true(desc.defaults.enumerable);
+});
+
+test('noEnumConfig value', (t: GenericTestContext<any>) => {
+  t.is(Foo.prototype.noEnumConfig, 'a');
+});
+
+test('noEnumConfig configurable', (t: GenericTestContext<any>) => {
+  t.true(desc.noEnumConfig.configurable);
+});
+
+test('noEnumConfig enumerable', (t: GenericTestContext<any>) => {
+  t.false(desc.noEnumConfig.enumerable);
+});
+
+test('enumNoConfig value', (t: GenericTestContext<any>) => {
+  t.is(Foo.prototype.enumNoConfig, 'b');
+});
+
+test('enumNoConfig configurable', (t: GenericTestContext<any>) => {
+  t.false(desc.enumNoConfig.configurable);
+});
+
+test('enumNoConfig enumerable', (t: GenericTestContext<any>) => {
+  t.true(desc.enumNoConfig.enumerable);
 });
