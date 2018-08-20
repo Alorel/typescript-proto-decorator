352626c167518e590836879b14b5b221fa3f0af2
Arturas <a.molcanovas@gmail.com>
2018-08-20 20:59:38 +0100

    fix(package): Add mode detailed file paths

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,32 @@
 const {join} = require('path');
+const merge = require('lodash/merge');
+const cloneDeep = require('lodash/cloneDeep');
 
-module.exports = {
+const base = {
   target: 'web',
   entry: join(__dirname, 'src', 'ProtoDecorator.ts'),
   output: {
     path: join(__dirname, 'dist', 'umd'),
-    filename: 'ProtoDecorator.js',
     libraryTarget: 'umd',
     library: 'ProtoDecorator'
   },
+  devtool: 'none',
   module: {
     rules: [
       {
         test: /\.ts$/,
         use: [{
-          loader: 'awesome-typescript-loader',
+          loader: 'ts-loader',
           options: {
-            configFileName: './tsconfig.esm5.json',
-            useCache: true,
-            cacheDirectory: join(__dirname, 'node_modules', '.cache', '.awcache')
+            configFile: join(__dirname, 'tsconfig.esm5.json'),
           }
         }]
       }
     ]
-  },
-  mode: 'production'
+  }
 };
+
+module.exports = [
+  merge(cloneDeep(base), {mode: 'development', output: {filename: 'ProtoDecorator.js'}}),
+  merge(cloneDeep(base), {mode: 'production', output: {filename: 'ProtoDecorator.min.js'}})
+];
